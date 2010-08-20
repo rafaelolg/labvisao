547ea5a047a339a952ce490793ec5190fd73c176
Rafael Lopes <rafaelolg@gmail.com>
2010-08-20 15:43:56 -0300

    Calculates the L2 norm of two images using opencv function.

--- similar.cpp
+++ similar.cpp
@@ -1,16 +1,54 @@
 #include "cv.h"
 #include "highgui.h"
+#include "stdio.h"
+
+using namespace std;
+
+
+
 
 using namespace cv;
 
+float histogram_distance(Mat a, Mat  b);
+
 int main( int argc, char** argv )
 {
-    const char* imagename = argc > 1 ? argv[1] : "lena.jpg";
-    Mat img = imread(imagename);
-    if( !img.data ) // check if the image has been loaded properly
+    const char * imga_name = argv[1];
+    const char * imgb_name = argv[2];
+
+    Mat imga = imread(imga_name);
+    Mat imgb = imread(imgb_name);
+
+    if( !imga.data || ! imgb.data) 
         return -1;
 
-    imshow("image", img);
+
+    imshow("A", imga);
+    imshow("B", imgb);
+
     waitKey();
+    float d = histogram_distance(imga, imgb);
+    printf("d = %f\n", d);
+    
+
+
+
     return 0;
 }
+
+
+
+float histogram_distance(Mat a, Mat  b) {
+    MatND  hista, histb;
+    int histsize[] = {256, 256, 256};
+    int channels[] = {0, 1, 2};
+    float range[] = {0, 256};
+    const float * ranges[] = {range,range,range};
+
+    calcHist(&a, 1, channels, Mat(), hista, 2, histsize, ranges, true, false);
+    calcHist(&b, 3, channels, Mat(), histb, 2, histsize, ranges, true, false);
+    // return compareHist(hista, hista, CV_COMP_BHATTACHARYYA);
+    return norm(hista, histb);
+}
+
+
